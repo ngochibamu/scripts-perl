3d593046036f87a1d969f99d73a49ed0725fa169
ngochibamu <ngoni.chibamu@gmail.com>
2017-10-25 01:01:45 +0200

    Datetime Conversion

---
+++ autotime_v001.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl6
+sub MAIN(Int $timestamp) {
+	
+	my $dt = DateTime.new(+$timestamp);
+	if $dt.Date.DateTime == $dt {
+		say $dt.Date
+	}
+	else {
+		say $dt;
+	}
+}
--- sudoku_v001.pl
+++ sudoku_v001.pl
@@ -1,13 +1,11 @@
 use v6;
 my $sudoku = '000000075000080094000500600010000200000900057006003040001000023080000006063240000';
-$sudoku .= trans('0' => ' ');
+$sudoku .= trans: '0' => ' ';
 
-my $separator = '+---+---+---+';
-my $index = 0;
+constant $separator = '+---+---+---+';
+
 for $sudoku.comb(9) -> $line {
-	if $index++ %% 3 {
-		say $separator;
-	}
+	say $separator if $++ %% 3;
 	say '|', $line.comb(3).join('|'), '|';
 }
 say $separator;
