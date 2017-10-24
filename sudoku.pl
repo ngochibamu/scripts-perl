fc083c7ec8c1b8b5adebf7b5c679ecd56090f827
ngochibamu <ngoni.chibamu@gmail.com>
2017-10-24 10:03:03 +0200

    Added bars

--- sudoku.pl
+++ sudoku.pl
@@ -1,5 +1,13 @@
 use v6;
 my $sudoku = '000000075000080094000500600010000200000900057006003040001000023080000006063240000';
-for 0..8 -> Int $line-number {
-	say substr $sudoku, $line-number * 9, 9;
+$sudoku .= trans('0' => ' ');
+
+sub chunks(Str $s, Int $chars){
+	gather loop (my $idx = 0; $idx < $s.chars; $idx += $chars){
+		take substr($s, $idx, $chars);
+	}
+}
+
+for chunks($sudoku, 9) -> $line {
+	say '|', chunks($line, 3).join('|'), '|';
 }
